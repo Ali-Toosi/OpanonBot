fd95888dd85b5252941a5b362688bb9bc2dd023b
Ali Toosi <alirezakhoshghalb@ymail.com>
2024-05-04 21:48:28 +1000

    Move get_link command higher in the order

--- src/chalicelib/bot_handlers/__init__.py
+++ src/chalicelib/bot_handlers/__init__.py
@@ -4,10 +4,10 @@ Import all handler files here so their functions would be registered.
 """
 
 from .start import *
+from .get_link import *
 from .chat import *
 from .block import *
 from .delete_link import *
 from .feature_request import *
 from .new_link import *
-from .get_link import *
 from .catch_all import *
